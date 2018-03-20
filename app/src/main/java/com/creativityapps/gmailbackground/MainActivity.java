77616c398c9efb714fa8eaf0e5c4c6c69f19bf01
Waseem Arain <waseemarain4u@gmail.com>
2018-03-20 13:24:54 +0500

    Update
    Upadat gradle repalce button

--- app/src/main/java/com/creativityapps/gmailbackground/MainActivity.java
+++ app/src/main/java/com/creativityapps/gmailbackground/MainActivity.java
@@ -1,12 +1,12 @@
 package com.creativityapps.gmailbackground;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import com.creativityapps.gmailbackgroundlibrary.BackgroundMail;
 
@@ -19,10 +19,18 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                sendTestEmail();
+            }
+        });*/
+
+        Button button= (Button) findViewById(R.id.fab);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 sendTestEmail();
             }
         });
